62e4af36a6dd2813e5473adc9b8cf80846981dd8
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-03 13:24:29 -0700

    update app to use promises

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs').promises;
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+const eventNames = {
+  READ: 'READ',
+  CONVERT: 'CONVERT',
+  TO_BUFFER: 'TO_BUFFER',
+  WRITE: 'WRITE',
+  ERROR: 'ERROR',
+};
+
+const pRead = (file) => {
+  return fs.readFile(file).catch((error) => {
+    throw error;
+  });
+};
+
+const sConvert = (fileBuffer) => {
+  return fileBuffer.toString().toUpperCase();
+};
+
+const bCreateBuffer = (string) => {
+  return Buffer.from(string);
+};
+
+const pWrite = (file, buffer) => {
+  return fs.writeFile(file, buffer).catch((error) => {
+    throw error;
+  });
+};
 
 const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
+  pRead(file)
+    .then((buffer) => {
+      // event here
+      let text = sConvert(buffer);
+      return pWrite(file, bCreateBuffer(text)); // emit next event
+    })
+    .then(() => {
+      // event here
+      console.log(`${file} saved`); // emit console log event
+    })
+    .catch((error) => {
+      throw error;
     });
-  });
 };
 
 let file = process.argv.slice(2).shift();
 alterFile(file);
+
+eventEmitter.on(eventNames.READ, file => pRead(file));
+eventEmitter.on(eventNames.CONVERT, buffer => sConvert(buffer));
+eventEmitter.on(eventNames.TO_BUFFER, string => bCreateBuffer(string));
+eventEmitter.on(eventNames.WRITE, (file, buffer) => pWrite(file, buffer));
+
+eventEmitter.on(eventNames.ERROR, (error) => {
+  throw error;
+});
+
